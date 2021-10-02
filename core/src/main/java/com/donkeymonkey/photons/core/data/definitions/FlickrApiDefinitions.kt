442cf39c6f5ceaddda0902ad4f0165d29c93f2af
pericles <pericleskb@gmail.com>
2021-10-02 23:57:01 +0300

    create main architecture and first files

---
+++ core/src/main/java/com/donkeymonkey/photons/core/data/definitions/FlickrApiDefinitions.kt
@@ -0,0 +1,7 @@
+package com.donkeymonkey.photons.core.data.definitions
+
+object FlickrApiDefinitions {
+    val flickrUrl = "https://www.flickr.com/services/rest/"
+    val flickrApiKey = "f8faf1692fcfcc59837f238c1c589585"
+    val flickrSecret = "0e42c8c78161304d"
+}
---
+++ core/src/main/java/com/donkeymonkey/photons/core/domain/repositories/PhotosRepository.kt
@@ -0,0 +1,5 @@
+package com.donkeymonkey.photons.core.domain.repositories
+
+interface PhotosRepository {
+    fun getPhotos()
+}
